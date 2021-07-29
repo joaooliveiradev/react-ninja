20b9fdc64f8b759e969eca792b38874a3039984b
joaooliveiradev <joaooliveira1775@gmail.com>
2021-07-28 21:24:47 -0300

    some improvements

--- modulo-02/applications/github-app/src/utils/pagination/index.js
+++ modulo-02/applications/github-app/src/utils/pagination/index.js
@@ -1,37 +1,42 @@
 'use strict';
-
 import { render } from "react-dom";
-
 const pagination = ({ total, activePage }) => {
     if (activePage > total) throw new RangeError('activePage is invalid');
     if (total <= 5) {
         return Array.from({ length: total }, (_, i) => (i + 1))
     }
     const totalPages = Array.from({ length: total }, (items, i) => (i + 1), '...');
-    //console.log(Number(array.length - (activePage + 1)))
-    //[1,'..',4,5,6,'..',10]
-
-
-    //pega o active page e coloca - o ultimo item - 1 para achar o range entre esses dois, e se for maior do que dois, vai
-    //cair os numeros que passaram e você substitui por ...
-    //talvez n precise vc usar um for para pegar o range dos numeros
-    console.log(activePage)
-    for (let i = totalPages[activePage - 1]; i < totalPages.length; i++) {
-        if (Math.abs(totalPages[i] - totalPages.length) >= 2) {
-            console.log(totalPages[i])
-        }
+    const rightActivePage = activePage + 1;
+    const leftActivePage = activePage - 1;
+    let result;
+    let rightRange;
+    let leftRange;
+    let leftRangeArr;
+    let rightRangeArr;
+    //Faz o Range entre dois pontos de vetores. 
+    const makeRange = (sideA, sideB) => {
+        return [...totalPages.slice(sideA, sideB)]
     }
-
-    return [1, 2, 3, 4, "...", 6, 7]
-
-    // pegar número atual array[activePage - 1];
-    //primeiros fazer os testes depois mudar o return para retornar as divs e fazer os testes visuais
-    // pense em active page e mudança de estado, active page pode ter uma classe css com uma cor diferente
-    // para o usuario pensar que está mudando.
-    // precisamos fazer um map em array para gerar uma lista com todas as posições da paginação.
-
-    return array
-
+    leftRangeArr = makeRange(totalPages[0], leftActivePage - 1);
+    rightRangeArr = makeRange(rightActivePage, totalPages.length - 1);
+    if (activePage == 1) {
+        rightRangeArr = makeRange(rightActivePage + 1, totalPages.length - 1);
+        result = totalPages.filter(item => !rightRangeArr.includes(item));
+        result.splice(rightActivePage + 1, 0, '...');
+        return result
+    } else if (leftRangeArr.length >= 2 && rightRangeArr.length >= 2) {
+        let leftResult = totalPages.filter(item => !leftRangeArr.includes(item))
+        result = leftResult.filter(item => !rightRangeArr.includes(item))
+        result.splice(totalPages[0], 0, '...').concat(result.splice(-1, 0, '...'));
+        return result
+    } else if (leftRangeArr.length >= 2) {
+        result = totalPages.filter(item => !leftRangeArr.includes(item));
+        result.splice(totalPages[0], 0, '...');
+        return result;
+    } else if (rightRangeArr.length >= 2) {
+        result = totalPages.filter(item => !rightRangeArr.includes(item));
+        result.splice(rightActivePage, 0, '...');
+        return result;
+    }
 }
-
 export default pagination
--- modulo-02/applications/github-app/src/utils/pagination/pagination.test.js
+++ modulo-02/applications/github-app/src/utils/pagination/pagination.test.js
@@ -24,13 +24,34 @@ test('pagination({total: 2, activePage: 3}) should return an RangeError with the
     }
     expect(error.name).to.be.equal('RangeError')
 })
-// test('pagination({total: 5, activePage: 1}) should return [1,2,3,4,5', () => {
-//     expect(pagination({ total: 5, activePage: 1 })).to.be.deep.equal([1, 2, 3, 4, 5])
-// })
-// test('pagination({total: 6, activePage: 1}) should return [1,2,3,"...",6]', () => {
-//     expect(pagination({ total: 6, activePage: 1 })).to.be.deep.equal([1, 2, 3, "...", 6])
-// })
-test('pagination({total: 7, activePage: 3}) should return [1,2,3,4,"...",7]', () => {
-    expect(pagination({ total: 10, activePage: 5 })).to.be.deep.equal([1, '...', 4, 5, "...", 10])
+test('pagination({total: 5, activePage: 1}) should return [1,2,3,4,5]', () => {
+    expect(pagination({ total: 5, activePage: 1 })).to.be.deep.equal([1, 2, 3, 4, 5])
+})
+test('pagination({total: 6, activePage: 1}) should return [1,2,3,"...",6]', () => {
+    expect(pagination({ total: 6, activePage: 1 })).to.be.deep.equal([1, 2, 3, "...", 6])
+})
+test('pagination({total: 7, activePage: 3}) should return [1,2,3,4, "...",7]', () => {
+    expect(pagination({ total: 7, activePage: 3 })).to.be.deep.equal([1, 2, 3, 4, "...", 7])
+})
+test('pagination({total: 9, activePage: 2}) should return [1, 2, 3, "...", 9]', () => {
+    expect(pagination({ total: 9, activePage: 2 })).to.be.deep.equal([1, 2, 3, "...", 9])
+})
+test('pagination({total: 10, activePage: 5}) should return [1,"...", 4,5,6,"...", 10]', () => {
+    expect(pagination({ total: 10, activePage: 5 })).to.be.deep.equal([1, '...', 4, 5, 6, "...", 10])
+})
+test('pagination({total: 30, activePage: 25}) should return [1,"...", 24,25,26,"...", 30]', () => {
+    expect(pagination({ total: 30, activePage: 25 })).to.be.deep.equal([1, '...', 24, 25, 26, "...", 30])
+})
+test('pagination({total: 10, activePage: 7}) should return [1,...,6,7,8,9,10]', () => {
+    expect(pagination({ total: 10, activePage: 7 })).to.be.deep.equal([1, '...', 6, 7, 8, 9, 10])
+})
+test('pagination({total: 10, activePage: 9}) should return [1,...8,9,10]', () => {
+    expect(pagination({ total: 10, activePage: 9 })).to.be.deep.equal([1, '...', 8, 9, 10])
+})
+test('pagination({total: 8, activePage: 5}) should return [1, ..., 4, 5, 6, 7, 8]', () => {
+    expect(pagination({ total: 8, activePage: 5 })).to.be.deep.equal([1, '...', 4, 5, 6, 7, 8])
 })
 
+test('pagination({total: 100, activePage:50 }) should return [1,"...", 49, 50, 51,"...", 100]', () => {
+    expect(pagination({ total: 100, activePage: 50 })).to.be.deep.equal([1, '...', 49, 50, 51, "...", 100])
+})
